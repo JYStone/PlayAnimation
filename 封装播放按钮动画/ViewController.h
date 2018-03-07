e821903bf0a8438d6fb6ac3f2caef342b161e9cc
jinjingyuan <jinjingyuan@quhuanqian.cn>
2018-03-07 15:35:53 +0800

    Initial Commit

---
+++ 封装播放按钮动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  封装播放按钮动画
+//
+//  Created by jy on 2018/3/7.
+//  Copyright © 2018年 M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 封装播放按钮动画/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  封装播放按钮动画
+//
+//  Created by jy on 2018/3/7.
+//  Copyright © 2018年 M. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
